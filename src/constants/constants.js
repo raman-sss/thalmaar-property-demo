6d105da11d24764c8580a18aff40081d13f89253
raman-sss <ramanm.sss@outlook.com>
2025-05-30 19:50:11 +0530

    Changed cloud account

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const url= 'http://54.167.163.206:5000';
-// export const url= 'https://thal-demo.dux-vitae.com';
+// export const url= 'http://54.167.163.206:5000';
+export const url= 'https://thal-demo.dux-vitae.com';
 // export const url= 'http://127.0.0.1:5000';
